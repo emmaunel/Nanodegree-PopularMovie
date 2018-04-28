0071a192b42d39416c33745f04e327e349572e01
Emmanuel <aadewale123@gmail.com>
2018-04-28 17:20:55 -0400

    Adding adapter

--- app/src/main/java/com/wordpress/ayo218/popularmovie/adapter/MovieAdapter.java
+++ app/src/main/java/com/wordpress/ayo218/popularmovie/adapter/MovieAdapter.java
@@ -1,10 +1,21 @@
 package com.wordpress.ayo218.popularmovie.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    private Context context;
+    private ArrayList<Movie> movies;
+
+    public MovieAdapter(Context context, ArrayList<Movie> movies) {
+        this.context;
+        this.movies;
+    }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
