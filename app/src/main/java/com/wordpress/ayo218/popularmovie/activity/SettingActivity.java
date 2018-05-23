fa763d9117210059747b1bd271e0008a4bd28a9a
Emmanuel <aadewale123@gmail.com>
2018-05-23 06:40:34 -0400

    Working on Settings

--- app/src/main/java/com/wordpress/ayo218/popularmovie/activity/SettingActivity.java
+++ app/src/main/java/com/wordpress/ayo218/popularmovie/activity/SettingActivity.java
@@ -1,11 +1,14 @@
 package com.wordpress.ayo218.popularmovie.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 import com.wordpress.ayo218.popularmovie.R;
+import com.wordpress.ayo218.popularmovie.fragment.SettingFragment;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -17,8 +20,16 @@ public class SettingActivity extends AppCompatActivity {
         if (getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+        initFragment();
     }
 
+    private void initFragment() {
+        SettingFragment fragment = new SettingFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.activity_setting, fragment);
+        transaction.commit();
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.home) {
