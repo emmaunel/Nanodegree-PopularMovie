ade73d0a5e9c264e003f1f10032aede214b9f04c
ayocode <aadewale123@gmail.com>
2018-05-25 11:34:40 -0400

    Remove API_Key

--- app/src/main/java/com/wordpress/ayo218/popularmovie/Constants.java
+++ app/src/main/java/com/wordpress/ayo218/popularmovie/Constants.java
@@ -1,7 +1,7 @@
 package com.wordpress.ayo218.popularmovie;
 
 public class Constants{
-    public static final String API_KEY = "38d7b13a8ee4e5e03842bd928ffb42ff";
+    public static final String API_KEY = "YOUR_API_KEY";
 
     public static final String BASE_IMAGE_URL = "http://image.tmdb.org/t/p/w780/";
 
